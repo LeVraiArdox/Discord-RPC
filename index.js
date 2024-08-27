d11d98b1f21a4f9d624f87b87285c67a61075a77
LeVraiArdox <adrian.arjoca18@gmail.com>
2024-08-27 19:18:40 +0200

    now waiting for Discord

--- index.js
+++ index.js
@@ -1,121 +1,152 @@
-const rpc = require('discord-rpc');
-const os = require('os');
-const exec = require('child_process').exec;
+const rpc = require("discord-rpc");
+const os = require("os");
+const exec = require("child_process").exec;
 
-const clientId = 'YOUR_ID'; // Replace this
-
+const clientId = "YOUR_ID"; // Replace this
 
 rpc.register(clientId);
 
-const client = new rpc.Client({ transport: 'ipc' });
+const client = new rpc.Client({ transport: "ipc" });
 
-let activeEditor = '';
+let activeEditor = "";
 let isListeningToSpotify = false;
-let currentTrack = ''; 
+let currentTrack = "";
 
-client.on('ready', () => {
-    console.log('Discord RPC is ready.');
+function isDiscordRunning() {
+  return new Promise((resolve) => {
+    exec("pgrep -x Discord", (err, stdout) => {
+      resolve(stdout.trim().length > 0);
+    });
+  });
+}
 
-    function checkIfCoding() {
+async function waitForDiscord() {
+  let discordRunning = await isDiscordRunning();
+  while (!discordRunning) {
+    console.log("Discord is not running. Waiting...");
+    await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for 5 seconds
+    discordRunning = await isDiscordRunning();
+  }
+}
+
+client.on("ready", () => {
+  console.log("Discord RPC is ready.");
+
+  function checkIfCoding() {
     return new Promise((resolve) => {
-        const editors = ['code', 'subl', 'idea', 'atom', 'vim', 'emacs', 'nvim', 'lite-xl'];
-        const checks = editors.map(editor => {
-            return new Promise((res) => {
-                exec(`pgrep -x ${editor}`, (err, stdout) => {
-                    if (stdout.trim().length > 0) {
-                        activeEditor = editor; // Set the active editor
-                    }
-                    res(stdout.trim().length > 0);
-                });
-            });
+      const editors = [
+        "code",
+        "subl",
+        "idea",
+        "atom",
+        "vim",
+        "emacs",
+        "nvim",
+        "lite-xl",
+      ];
+      const checks = editors.map((editor) => {
+        return new Promise((res) => {
+          exec(`pgrep -x ${editor}`, (err, stdout) => {
+            if (stdout.trim().length > 0) {
+              activeEditor = editor; // Set the active editor
+            }
+            res(stdout.trim().length > 0);
+          });
         });
+      });
 
-        Promise.all(checks).then(results => {
-            resolve(results.some(result => result));
-            });
-        });
-    }
+      Promise.all(checks).then((results) => {
+        resolve(results.some((result) => result));
+      });
+    });
+  }
 
-    function checkIfListeningToSpotify() {
+  function checkIfListeningToSpotify() {
     return new Promise((resolve) => {
-        exec('playerctl -l', (err, stdout) => {
-            const players = stdout.trim().split('\n');
-            isListeningToSpotify = players.includes('spotify');
-            resolve(isListeningToSpotify);
-            });
-        });
+      exec("playerctl -l", (err, stdout) => {
+        const players = stdout.trim().split("\n");
+        isListeningToSpotify = players.includes("spotify");
+        resolve(isListeningToSpotify);
+      });
+    });
+  }
+
+  function getSpotifyTrackInfo() {
+    return new Promise((resolve) => {
+      exec(
+        'playerctl metadata --format "{{ title }} - {{ artist }}"',
+        (err, stdout) => {
+          if (err || !stdout.trim()) {
+            currentTrack = "Unknown Track";
+          } else {
+            currentTrack = stdout.trim();
+          }
+          resolve(currentTrack);
+        },
+      );
+    });
+  }
+
+  async function setActivity() {
+    const isCoding = await checkIfCoding();
+    await checkIfListeningToSpotify();
+
+    if (isListeningToSpotify) {
+      await getSpotifyTrackInfo();
+      client.setActivity({
+        details: `Listening Spotify`,
+        state: currentTrack,
+        largeImageKey: "axos",
+        largeImageText: "AxOS",
+        smallImageKey: "spotify", // Ensure you have uploaded this asset to your Discord application
+        smallImageText: "Spotify",
+        instance: false,
+        buttons: [
+          {
+            label: "Install AxOS",
+            url: "https://www.axos-project.com",
+          },
+        ],
+      });
+    } else if (isCoding) {
+      client.setActivity({
+        details: `Coding on ${activeEditor}`, // Static title
+        state: `Uptime: ${Math.round(os.uptime() / 60)} minutes`,
+        largeImageKey: "axos", // Ensure you have uploaded this asset to your Discord application
+        largeImageText: "AxOS",
+        smallImageKey: "vscode", // Ensure you have uploaded this asset to your Discord application
+        smallImageText: "Coding",
+        instance: false,
+        buttons: [
+          {
+            label: "Install AxOS",
+            url: "https://www.axos-project.com",
+          },
+        ],
+      });
+    } else {
+      client.setActivity({
+        details: "Is working (or not)", // Static title
+        state: `Uptime: ${Math.round(os.uptime() / 60)} minutes`,
+        largeImageKey: "axos", // Ensure you have uploaded this asset to your Discord application
+        largeImageText: "AxOS",
+        instance: false,
+        buttons: [
+          {
+            label: "Install AxOS",
+            url: "https://www.axos-project.com",
+          },
+        ],
+      });
     }
+  }
 
-    function getSpotifyTrackInfo() {
-        return new Promise((resolve) => {
-            exec('playerctl metadata --format "{{ title }} - {{ artist }}"', (err, stdout) => {
-                if (err || !stdout.trim()) {
-                    currentTrack = 'Unknown Track';
-                } else {
-                    currentTrack = stdout.trim();
-                }
-                resolve(currentTrack);
-            });
-        });
-    }
-
-
-
-    async function setActivity() {
-        const isCoding = await checkIfCoding();
-        await checkIfListeningToSpotify();
-
-        if (isListeningToSpotify) {
-            await getSpotifyTrackInfo();
-            client.setActivity({
-                details: `Listening Spotify`,
-                state: currentTrack,
-                largeImageKey: 'axos',
-                largeImageText: 'AxOS',
-                smallImageKey: 'spotify', // Ensure you have uploaded this asset to your Discord application
-                smallImageText: 'Spotify',
-                instance: false,
-                buttons: [
-                   {
-                      label: "Install AxOS",
-                      url: "https://www.axos-project.com"
-                   }]
-        });
-        }
-        else if (isCoding) {
-            client.setActivity({
-                details: `Coding on ${activeEditor}`, // Static title
-                state: `Uptime: ${Math.round(os.uptime() / 60)} minutes`,
-                largeImageKey: 'axos', // Ensure you have uploaded this asset to your Discord application
-                largeImageText: 'AxOS',
-                smallImageKey: 'vscode', // Ensure you have uploaded this asset to your Discord application
-                smallImageText: 'Coding',
-                instance: false,
-                buttons: [
-                   {
-                      label: "Install AxOS",
-                      url: "https://www.axos-project.com"
-                   }]
-            });
-        } else {
-            client.setActivity({
-                details: 'Is working (or not)', // Static title
-                state: `Uptime: ${Math.round(os.uptime() / 60)} minutes`,
-                largeImageKey: 'axos', // Ensure you have uploaded this asset to your Discord application
-                largeImageText: 'AxOS',
-                instance: false,
-                buttons: [
-                   {
-                      label: "Install AxOS",
-                      url: "https://www.axos-project.com"
-                   }]
-            });
-        }
-    }
-
-    setActivity();
-    setInterval(setActivity, 5000); // Update presence every 5 seconds
+  setActivity();
+  setInterval(setActivity, 5000); // Update presence every 5 seconds
 });
 
-client.login({ clientId }).catch(console.error);
+(async () => {
+  await waitForDiscord(); // Wait for Discord to start
+  client.login({ clientId }).catch(console.error);
+})();
 
